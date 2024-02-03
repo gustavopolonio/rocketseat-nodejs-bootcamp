44c8fded8ac2da4441b117f9e97606288f58c7d9
gustavopolonio <gustavopolonio1@gmail.com>
2024-02-03 16:35:18 -0300

    http methods

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,6 +1,26 @@
 import http from 'node:http'
 
+// HTTP methods
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso no back-end
+
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+  
+  if (method === 'GET' && url === '/users') {
+    console.log('Listagem de usuários')
+  }
+
+  if (method === 'POST' && url === '/users') {
+    console.log('Criação de usuários')
+  }
+
   return res.end('Hello!')
 })
 
