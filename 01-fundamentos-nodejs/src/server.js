62b0a5eaac0329584d0dc91fc01f315bec467a62
gustavopolonio <gustavopolonio1@gmail.com>
2024-02-06 11:30:17 -0300

    req body with json

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -14,9 +14,21 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
-  
+
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
+
   if (method === 'GET' && url === '/users') {
     return res
       .setHeader('Content-Type', 'application/json')
@@ -24,10 +36,12 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, age } = req.body
+
     users.push({
       id: 1,
-      name: 'Gustavo',
-      email: 'gustavo@teste.com'
+      name,
+      age
     })
 
     return res.writeHead(201).end()
