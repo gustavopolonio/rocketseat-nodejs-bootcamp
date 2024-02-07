6ed990fa59185439ec7feb28e07a7a2000de3429
gustavopolonio <gustavopolonio1@gmail.com>
2024-02-07 09:34:52 -0300

    middleware intro

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,5 +1,7 @@
 import http from 'node:http'
 
+import { json } from './middlewares/json.js'
+
 // HTTP methods
 // GET, POST, PUT, PATCH, DELETE
 
@@ -17,22 +19,10 @@ const users = []
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  const buffers = []
-
-  for await (const chunk of req) {
-    buffers.push(chunk)
-  }
-
-  try {
-    req.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    req.body = null
-  }
+  await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-      .setHeader('Content-Type', 'application/json')
-      .end(JSON.stringify(users))
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
