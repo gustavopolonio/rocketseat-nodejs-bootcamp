159c0927d3987118c694cc9b136d71b4e5c7bb36
gustavopolonio <gustavopolonio1@gmail.com>
2024-02-07 11:08:58 -0300

    adding uuid

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
 
 import { json } from './middlewares/json.js'
 import { Database } from './database.js'
@@ -32,7 +33,7 @@ const server = http.createServer(async (req, res) => {
     const { name, age } = req.body
 
     const user = {
-      id: 1,
+      id: randomUUID(),
       name,
       age
     }
