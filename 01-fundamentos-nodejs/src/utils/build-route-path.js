bb4ecac2a595a80fbe073509d05e4c18639bce31
gustavopolonio <gustavopolonio1@gmail.com>
2024-03-02 10:24:58 -0300

    feat: getting query params

--- 01-fundamentos-nodejs/src/utils/build-route-path.js
+++ 01-fundamentos-nodejs/src/utils/build-route-path.js
@@ -2,7 +2,7 @@ export function buildRoutePath(path) {
   const routeParamsRegex = /:([a-zA-z]+)/g
 
   const pathWithParams = path.replaceAll(routeParamsRegex, '(?<$1>[a-z0-9\-_]+)')
-  const pathRegex = new RegExp(`^${pathWithParams}`)
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
 
   return pathRegex
 }
---
+++ 01-fundamentos-nodejs/src/utils/extract-query-params.js
@@ -0,0 +1,10 @@
+// '?search=Gustavo&page=2'
+
+export function extractQueryParams(query) {
+  return query.substring(1).split('&').reduce((queryParams, param) => {
+    const [key, value] = param.split('=')
+    queryParams[key] = value
+    
+    return queryParams
+  }, {})
+}
