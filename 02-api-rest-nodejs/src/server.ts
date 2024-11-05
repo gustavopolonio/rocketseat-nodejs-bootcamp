bec6c1915663a1b9a89d2e9d32fee4a3222715f6
Gustavo <gustavopolonio1@gmail.com>
2024-11-05 12:34:22 -0300

    database setup

--- 02-api-rest-nodejs/src/server.ts
+++ 02-api-rest-nodejs/src/server.ts
@@ -1,15 +1,21 @@
 import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
-app.get('/ping', () => {
-  return 'pong'
+app.get('/', async () => {
+  const tables = await knex('transactions')
+    .select('*')
+    .where('amount', 1000.111)
+
+  return tables
 })
 
 app
   .listen({
-    port: 3000,
+    port: env.PORT,
   })
   .then(() => {
-    console.log('Server running')
+    console.log(`Server running - PORT: ${env.PORT}`)
   })
