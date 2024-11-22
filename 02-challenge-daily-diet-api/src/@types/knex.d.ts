e4317b18df282b37e9651b20b39c0da6be0c313c
Gustavo <gustavopolonio1@gmail.com>
2024-11-21 22:11:53 -0300

    migration: create users table

---
+++ 02-challenge-daily-diet-api/src/@types/knex.d.ts
@@ -0,0 +1,23 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Knex } from 'knex'
+
+declare module 'knex/types/tables' {
+  interface Tables {
+    users: {
+      id: string
+      name: string
+      email: string
+      created_at: string
+    }
+
+    meals: {
+      id: string
+      user_id: string
+      name: string
+      description: string
+      date_time: string
+      is_within_diet: boolean
+      session_id: string
+    }
+  }
+}
