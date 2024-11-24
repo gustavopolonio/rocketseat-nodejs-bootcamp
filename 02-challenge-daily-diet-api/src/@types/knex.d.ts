efcd3033f82015c58cbe7fafdd2e480082cf1cd6
Gustavo <gustavopolonio1@gmail.com>
2024-11-24 20:31:52 -0300

    migration: add session_id column to users table

--- 02-challenge-daily-diet-api/src/@types/knex.d.ts
+++ 02-challenge-daily-diet-api/src/@types/knex.d.ts
@@ -5,6 +5,7 @@ declare module 'knex/types/tables' {
   interface Tables {
     users: {
       id: string
+      session_id: string
       name: string
       email: string
       created_at: string
