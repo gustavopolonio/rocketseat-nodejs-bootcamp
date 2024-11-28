6121f841a8d5f124b1f5782d3de726062e4b8f6f
Gustavo <gustavopolonio1@gmail.com>
2024-11-28 17:04:36 -0300

    fix(migration): remove unused await operator

--- 02-challenge-daily-diet-api/db/migrations/20241124232403_add-session-id-column-to-users-table.ts
+++ 02-challenge-daily-diet-api/db/migrations/20241124232403_add-session-id-column-to-users-table.ts
@@ -1,13 +1,13 @@
 import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-  return await knex.schema.alterTable('users', (table) => {
+  return knex.schema.alterTable('users', (table) => {
     table.uuid('session_id').index().after('id')
   })
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return await knex.schema.alterTable('users', (table) => {
+  return knex.schema.alterTable('users', (table) => {
     table.dropColumn('session_id')
   })
 }
