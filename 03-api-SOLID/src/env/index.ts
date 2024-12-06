eb3add7611c43b856c6269edd7f217d6dd9dd4a0
Gustavo <gustavopolonio1@gmail.com>
2024-12-06 14:58:32 -0300

    chore: fix eslint errors

--- 03-api-SOLID/src/env/index.ts
+++ 03-api-SOLID/src/env/index.ts
@@ -3,7 +3,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['test', 'development', 'production']).default('production'),
-  PORT: z.coerce.number().default(3333)
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
