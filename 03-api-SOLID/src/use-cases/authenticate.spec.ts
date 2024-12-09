44afa895effb5168ba33177813bb59b4b33277d4
Gustavo <gustavopolonio1@gmail.com>
2024-12-09 18:10:37 -0300

    test(authenticate): add test for preventing authentication with wrong email

--- 03-api-SOLID/src/use-cases/authenticate.spec.ts
+++ 03-api-SOLID/src/use-cases/authenticate.spec.ts
@@ -1,10 +1,10 @@
-// it should not be able to authenticate with wrong email
 // it should not be able to authenticate with wrong password
 
 import { describe, expect, it } from 'vitest'
 import { hash } from 'bcryptjs'
 import { AuthenticateUseCase } from './authenticate'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 
 describe('Authenticate Use Case', () => {
   it('should be able to authenticate', async () => {
@@ -26,5 +26,17 @@ describe('Authenticate Use Case', () => {
     })
 
     expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should not be able to authenticate with wrong email', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    await expect(
+      sut.execute({
+        email: 'user1@test.test',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
   })
 })
