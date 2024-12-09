160b813ee866e8cffe4cc168926596849c5bb197
Gustavo <gustavopolonio1@gmail.com>
2024-12-09 18:06:14 -0300

    test(authenticate): add test for user athentication

---
+++ 03-api-SOLID/src/use-cases/authenticate.spec.ts
@@ -0,0 +1,30 @@
+// it should not be able to authenticate with wrong email
+// it should not be able to authenticate with wrong password
+
+import { describe, expect, it } from 'vitest'
+import { hash } from 'bcryptjs'
+import { AuthenticateUseCase } from './authenticate'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+
+describe('Authenticate Use Case', () => {
+  it('should be able to authenticate', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const sut = new AuthenticateUseCase(usersRepository)
+
+    const email = 'user1@test.test'
+    const password = '123456'
+
+    await usersRepository.create({
+      name: 'User test 01',
+      email,
+      password_hash: await hash(password, 6),
+    })
+
+    const { user } = await sut.execute({
+      email,
+      password,
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+})
