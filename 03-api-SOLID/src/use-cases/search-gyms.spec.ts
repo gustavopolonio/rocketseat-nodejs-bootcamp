2b6bcb9097159196e7d496e8d5b55794a6acb43b
Gustavo <gustavopolonio1@gmail.com>
2024-12-12 20:56:44 -0300

    test(gyms): add test for search paginated gyms by name

--- 03-api-SOLID/src/use-cases/search-gyms.spec.ts
+++ 03-api-SOLID/src/use-cases/search-gyms.spec.ts
@@ -45,4 +45,35 @@ describe('Search Gyms Use Case', () => {
       }),
     ])
   })
+
+  it('should be able to search paginated gyms by name', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        name: `Javascript Gym - ${i}`,
+        latitude: 0,
+        longitude: 0,
+      })
+    }
+
+    await gymsRepository.create({
+      name: 'Python Strong',
+      latitude: 0,
+      longitude: 0,
+    })
+
+    const { gyms } = await sut.execute({
+      query: 'javascript',
+      page: 2,
+    })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({
+        name: 'Javascript Gym - 21',
+      }),
+      expect.objectContaining({
+        name: 'Javascript Gym - 22',
+      }),
+    ])
+  })
 })
