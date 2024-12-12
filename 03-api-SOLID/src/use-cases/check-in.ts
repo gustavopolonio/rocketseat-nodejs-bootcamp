6549f6b9c3446f92c1fdf0f2632f5887e48ed59c
Gustavo <gustavopolonio1@gmail.com>
2024-12-12 11:07:40 -0300

    feat: check if gym exists on check ins use case

--- 03-api-SOLID/src/use-cases/check-in.ts
+++ 03-api-SOLID/src/use-cases/check-in.ts
@@ -1,5 +1,7 @@
 import { CheckInsRepository } from '@/repositories/check-ins-repository'
+import { GymsRepository } from '@/repositories/gyms-repository'
 import { CheckIn } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 interface CheckInUseCaseProps {
   userId: string
@@ -11,12 +13,21 @@ interface CheckInUseCaseResponse {
 }
 
 export class CheckInUseCase {
-  constructor(private checkInsRepository: CheckInsRepository) {}
+  constructor(
+    private checkInsRepository: CheckInsRepository,
+    private gymsRepository: GymsRepository,
+  ) {}
 
   async execute({
     userId,
     gymId,
   }: CheckInUseCaseProps): Promise<CheckInUseCaseResponse> {
+    const gym = await this.gymsRepository.findById(gymId)
+
+    if (!gym) {
+      throw new ResourceNotFoundError()
+    }
+
     const checkInOnSameDate = await this.checkInsRepository.findByUserIdOnDate(
       userId,
       new Date(),
