2a0db6ed7704983fdbab017171378357aa1cdcee
Gustavo <gustavopolonio1@gmail.com>
2024-12-12 21:36:51 -0300

    test(gyms): add test for get nearby gyms

---
+++ 03-api-SOLID/src/use-cases/get-nearby-gyms.spec.ts
@@ -0,0 +1,49 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { GetNearbyGymsUseCase } from './get-nearby-gyms'
+import { GymsRepository } from '@/repositories/gyms-repository'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+
+let gymsRepository: GymsRepository
+let sut: GetNearbyGymsUseCase
+
+describe('Get Nearby Gyms Use Case', () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new GetNearbyGymsUseCase(gymsRepository)
+  })
+
+  it('should be able to get nearby gyms', async () => {
+    await gymsRepository.create({
+      name: 'Gym 01',
+      latitude: 0,
+      longitude: 0,
+    })
+
+    await gymsRepository.create({
+      name: 'Gym 02',
+      latitude: 0,
+      longitude: 0,
+    })
+
+    await gymsRepository.create({
+      name: 'Gym 03',
+      latitude: 10,
+      longitude: 10,
+    })
+
+    const { gyms } = await sut.execute({
+      userLatitude: 0,
+      userLongitude: 0,
+    })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({
+        name: 'Gym 01',
+      }),
+      expect.objectContaining({
+        name: 'Gym 02',
+      }),
+    ])
+  })
+})
