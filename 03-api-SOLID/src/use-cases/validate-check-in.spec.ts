dd9be815905747b06029da26ed259fb5c92d129e
Gustavo <gustavopolonio1@gmail.com>
2024-12-14 20:27:04 -0300

    test(checkIn): add test for preventing validate check-in after 21 minutes or more of its creation

--- 03-api-SOLID/src/use-cases/validate-check-in.spec.ts
+++ 03-api-SOLID/src/use-cases/validate-check-in.spec.ts
@@ -1,22 +1,34 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { ValidateCheckInUseCase } from './validate-check-in'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { ExpiredCheckInValidationError } from './errors/expired-check-in-validation-error'
 
 let checkInsRepository: InMemoryCheckInsRepository
 let sut: ValidateCheckInUseCase
 
 describe('Validate Check-in Use Case', () => {
   beforeEach(() => {
+    vi.useFakeTimers()
+
     checkInsRepository = new InMemoryCheckInsRepository()
     sut = new ValidateCheckInUseCase(checkInsRepository)
   })
 
+  afterEach(() => {
+    vi.useRealTimers()
+  })
+
   it('should be able to validate check-in', async () => {
+    vi.setSystemTime(new Date(2000, 0, 1, 10, 40))
+
     const createdCheckIn = await checkInsRepository.create({
       user_id: 'user-01',
       gym_id: 'gym-01',
     })
+
+    const twentyMinutesInMs = 1000 * 60 * 20
+    vi.advanceTimersByTime(twentyMinutesInMs)
 
     const { checkIn } = await sut.execute({
       checkInId: createdCheckIn.id,
@@ -32,5 +44,23 @@ describe('Validate Check-in Use Case', () => {
         checkInId: 'non-existing-id',
       }),
     ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+
+  it('should not be able to validate check-in after 21 minutes or more of its creation', async () => {
+    vi.setSystemTime(new Date(2000, 0, 1, 10, 40))
+
+    const checkIn = await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-01',
+    })
+
+    const twentyOneMinutesInMs = 1000 * 60 * 21
+    vi.advanceTimersByTime(twentyOneMinutesInMs)
+
+    await expect(
+      sut.execute({
+        checkInId: checkIn.id,
+      }),
+    ).rejects.toBeInstanceOf(ExpiredCheckInValidationError)
   })
 })
