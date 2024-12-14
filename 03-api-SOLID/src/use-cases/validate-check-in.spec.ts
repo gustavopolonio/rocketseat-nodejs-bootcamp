050712831c3c99aa45cd75d0c8944e3f80259b70
Gustavo <gustavopolonio1@gmail.com>
2024-12-14 19:55:57 -0300

    test(checkIn): add test for preventing validate unexisting check-in

--- 03-api-SOLID/src/use-cases/validate-check-in.spec.ts
+++ 03-api-SOLID/src/use-cases/validate-check-in.spec.ts
@@ -1,6 +1,7 @@
 import { beforeEach, describe, expect, it } from 'vitest'
 import { ValidateCheckInUseCase } from './validate-check-in'
 import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 let checkInsRepository: InMemoryCheckInsRepository
 let sut: ValidateCheckInUseCase
@@ -23,5 +24,13 @@ describe('Validate Check-in Use Case', () => {
 
     expect(checkIn.validated_at).toEqual(expect.any(Date))
     expect(checkInsRepository.items[0].validated_at).toEqual(expect.any(Date))
+  })
+
+  it('should not be able to validate unexisting check-in', async () => {
+    await expect(
+      sut.execute({
+        checkInId: 'non-existing-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
   })
 })
