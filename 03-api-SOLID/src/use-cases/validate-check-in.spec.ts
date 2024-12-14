40ac0695b168fa1aad18298d1c7bd8afc5464b5f
Gustavo <gustavopolonio1@gmail.com>
2024-12-14 19:55:08 -0300

    test(checkIn): add ter for validate check-in

---
+++ 03-api-SOLID/src/use-cases/validate-check-in.spec.ts
@@ -0,0 +1,27 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { ValidateCheckInUseCase } from './validate-check-in'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+
+let checkInsRepository: InMemoryCheckInsRepository
+let sut: ValidateCheckInUseCase
+
+describe('Validate Check-in Use Case', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    sut = new ValidateCheckInUseCase(checkInsRepository)
+  })
+
+  it('should be able to validate check-in', async () => {
+    const createdCheckIn = await checkInsRepository.create({
+      user_id: 'user-01',
+      gym_id: 'gym-01',
+    })
+
+    const { checkIn } = await sut.execute({
+      checkInId: createdCheckIn.id,
+    })
+
+    expect(checkIn.validated_at).toEqual(expect.any(Date))
+    expect(checkInsRepository.items[0].validated_at).toEqual(expect.any(Date))
+  })
+})
