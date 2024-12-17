8513c8d09deb2dbdb44de27e3be0c65d3b2cf7c3
Gustavo <gustavopolonio1@gmail.com>
2024-12-16 21:39:28 -0300

    refactor: gather users routes

--- 03-api-SOLID/src/app.ts
+++ 03-api-SOLID/src/app.ts
@@ -1,12 +1,12 @@
 import fastify from 'fastify'
 import fastifyJwt from '@fastify/jwt'
-import { appRoutes } from './http/routes'
 import { ZodError } from 'zod'
 import { env } from './env'
+import { usersRoutes } from './http/controllers/users/routes'
 
 export const app = fastify()
 
-app.register(appRoutes)
+app.register(usersRoutes)
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
