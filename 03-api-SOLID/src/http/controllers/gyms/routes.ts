2d9e50cad8876385c0710804bc4f8d52cd11d8c5
Gustavo <gustavopolonio1@gmail.com>
2024-12-17 15:23:36 -0300

    feat: add search gyms route

--- 03-api-SOLID/src/http/controllers/gyms/routes.ts
+++ 03-api-SOLID/src/http/controllers/gyms/routes.ts
@@ -1,9 +1,11 @@
 import { verifyJwt } from '@/http/middleware/verify-jwt'
 import { FastifyInstance } from 'fastify'
 import { create } from './create'
+import { search } from './search'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
 
+  app.get('/gyms', search)
   app.post('/gyms', create)
 }
