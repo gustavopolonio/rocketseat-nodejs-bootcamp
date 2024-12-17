daf873ea03f64ccab702e4d40e2b1f44efc33963
Gustavo <gustavopolonio1@gmail.com>
2024-12-16 21:54:02 -0300

    feat: add create gym route

--- 03-api-SOLID/src/app.ts
+++ 03-api-SOLID/src/app.ts
@@ -3,13 +3,16 @@ import fastifyJwt from '@fastify/jwt'
 import { ZodError } from 'zod'
 import { env } from './env'
 import { usersRoutes } from './http/controllers/users/routes'
+import { gymsRoutes } from './http/controllers/gyms/routes'
 
 export const app = fastify()
 
-app.register(usersRoutes)
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 })
+
+app.register(usersRoutes)
+app.register(gymsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
