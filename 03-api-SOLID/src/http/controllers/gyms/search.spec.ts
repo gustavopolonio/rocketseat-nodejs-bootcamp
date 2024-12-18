0a7bd0c974da0e9abb20269fdf9263b29c31a742
Gustavo <gustavopolonio1@gmail.com>
2024-12-18 12:44:07 -0300

    test(e2e): add test for retrieve gyms by name

---
+++ 03-api-SOLID/src/http/controllers/gyms/search.spec.ts
@@ -0,0 +1,58 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import { createGym } from '@/utils/test/create-gym'
+
+describe('Seach gyms (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to search gyms by name', async () => {
+    const name = 'Test 01'
+    const email = 'test01@test.com'
+    const password = '123456'
+
+    const { token } = await createAndAuthenticateUser(app, {
+      name,
+      email,
+      password,
+    })
+
+    await createGym(app, token, {
+      name: 'Gym 01',
+      description: null,
+      phone: null,
+      latitude: 0,
+      longitude: 0,
+    })
+
+    await createGym(app, token, {
+      name: 'Gym 02',
+      description: null,
+      phone: null,
+      latitude: 0,
+      longitude: 0,
+    })
+
+    const response = await request(app.server)
+      .get('/gyms/search')
+      .query({
+        query: '01',
+      })
+      .set('Authorization', `Bearer ${token}`)
+
+    expect(response.status).toEqual(200)
+    expect(response.body.gyms).toHaveLength(1)
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        name: 'Gym 01',
+      }),
+    ])
+  })
+})
